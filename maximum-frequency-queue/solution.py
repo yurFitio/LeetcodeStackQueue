5f51a50ab48ee597b76d4f5095d00d376eba3c54
Yurii Fito <fito.pn@ucu.edu.ua>
2025-04-08 15:40:02 +0300

    Add classes, make pop and push functions
     - Added Node and FreqStack classes\n - Implemented pop and push\n - Added some basic tests

---
+++ maximum-frequency-queue/solution.py
@@ -0,0 +1,46 @@
+class Node:
+    def __init__(self, value, next_node=None):
+        self.value = value
+        self.amount = 1
+        self.next = next_node
+
+    # def __repr__(self):
+    #     return f"Value: {self.value}, amount: {self.amount}, next: {self.next}"
+
+class FreqStack:
+
+    def __init__(self):
+        self.head = None
+
+    def push(self, val: int) -> None:
+        current = self.head
+        self.head = Node(val, self.head)
+        while current:
+            if current.value == self.head.value:
+                self.head.amount += current.amount
+                break
+            current = current.next
+
+    def pop(self) -> int:
+        if not self.head:
+            raise IndexError
+        previous, node_max = None, self.head
+        current = self.head
+        while current.next:
+            if current.next.amount > node_max.amount:
+                node_max = current.next
+                previous = current
+            current = current.next
+        if previous:
+            previous.next = previous.next.next
+        else:
+            self.head = self.head.next
+        return node_max.value
+
+# Your FreqStack object will be instantiated and called as such:
+if __name__ == '__main__':
+    obj = FreqStack()
+    for i in [5, 7, 5, 7, 4, 5]:
+        obj.push(i)
+    while True:
+        print(obj.pop())
